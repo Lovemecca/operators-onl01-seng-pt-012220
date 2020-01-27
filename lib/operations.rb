359ba383f8e78d527101d26794a3e70e1f6ef1e3
Mecca Jay <mecca.nicolej@gmail.com>
2020-01-27 03:47:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
  if speed < 40 or speed > 60
-   "TRUE"
+   true
  else  
-  "FALSE"
+  false
  end
 end
 
 
 def not_safe?(speed)
-	speed < 40 or speed > 60 ? "TRUE" : "False"
+	speed < 40 or speed > 60 ? true : false
 end
 	
 
